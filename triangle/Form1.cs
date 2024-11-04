1d883e4d6b5f41379bb56012d004e1de3f1978d3
opilane <opilane@HP-CZC2349HTS>
2024-11-04 15:47:15 +0200

    Add project files.

---
+++ triangle/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace triangle
+{
+
+    public partial class Form1 : Form
+    {
+        Button btn_start;
+
+        public Form1(){
+
+            Text = "töö kolmnurgaga";
+            //Icon = new Icon("Triangle.ico");
+            Button btn_start = new Button();
+            btn_start.Text = "kaivitada";
+            btn_start.BackColor = Color.Black;
+           // btn_start.Font = new Font("Arial", 28);
+            btn_start.Cursor = Cursors.Hand;
+            btn_start.FlatAppearance.BorderColor = Color.FromArgb(0, 192, 192);
+            btn_start.FlatAppearance.BorderSize = 10;
+            btn_start.FlatStyle = FlatStyle.Flat;
+            Controls.Add(btn_start);
+            double A = 3;
+            double B = 4;
+            double C = 5;
+            Triangle1 triangle = new Triangle1(A, B, C);
+
+            double perimeter = triangle.Perimeter();
+            double surface = triangle.Surface();
+
+  
+        }
+    }
+}
---
+++ triangle/triangle1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace triangle
+{
+    public class Triangle1
+    {
+        public double a, b, c;
+        public Triangle1(double a, double b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+        public string outputA()
+        {
+            return a.ToString();
+        }
+
+        public string outputB()
+        {
+            return b.ToString();
+        }
+
+        public string outputC()
+        {
+            return c.ToString();
+        }
+
+        public double Perimeter()
+        {
+            return a + b + c;
+        }
+
+        public double Surface()
+        {
+            double p = (a + b + c) / 2; 
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+
+        public double GetSetA
+        {
+            get { return a; }
+            set { a = value; }
+        }
+
+        public double GetSetB
+        {
+            get { return b; }
+            set { b = value; }
+        }
+
+        public double GetSetC
+        {
+            get { return c; }
+            set { c = value; }
+        }
+
+        public bool ExistTriangle
+        {
+            get
+            {
+                if ((a + b > c) && (a + c > b) && (b + c > a))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
